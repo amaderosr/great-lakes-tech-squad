d8cde76660ab48ebe6534c5fc3d7df5ad5e77ee5
Armando Madero <armandomadero@Armandos-MacBook-Pro-2.local>
2025-04-15 20:44:11 -0400

    🧠 Add CORS debug logger

--- server/index.js
+++ server/index.js
@@ -15,7 +15,15 @@ const corsOptions = {
   
   app.use(cors(corsOptions));
   app.options('*', cors(corsOptions)); // <-- handles preflight properly
-  
+
+  app.use((req, res, next) => {
+    console.log('[CORS DEBUG]', {
+      origin: req.headers.origin,
+      method: req.method
+    });
+    next(); // continue to next middleware or route
+  });
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
